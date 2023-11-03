d06abbed0d10b09537fb6959dde4da35d50ef666
Zhiguo Wang <36352982+emailwzg@users.noreply.github.com>
2023-11-03 22:45:57 +0800

    Update GBWTest.m

--- GBWTest.m
+++ GBWTest.m
@@ -2,7 +2,7 @@
 Generalized Beta wavelets
 Zhiguo Wang
 Xi'an Jiaotong University
-2023-09-113
+2023-09-13
 cite: Wang, Zhiguo, Bing Zhang, Jinghuai Gao, Qingzhen Wang, and Qing Huo Liu, 
 The wavelet transform with generalized Beta wavelets for seismic time-frequency analysis, 
 Geophysics, 2017, 82(4), O47-O56
